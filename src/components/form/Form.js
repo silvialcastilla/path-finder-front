44579ee28e6b4409df2a12a6e4cd86daef116e4d
Silvia <silope0302@gmail.com>
2020-07-16 15:44:54 +0200

    pc sil

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,81 +1,88 @@
-import React, {useState} from "react";
+import React from "react";
 import './Form.css'
 
 
-const Form = ({crearCoordenada}) => {
-
-    
-    const [coordenada, actualizarCoordenada] = useState({
-        origin: '',
-        destiny: '',
-        lat: '',
-        lng: ''
-    })
-    
-    const [ error, actualizarError ] = useState(false)
-    
-    const actualizarState = e => {
-        actualizarCoordenada({
-            ...coordenada,
-            [e.target.name]: e.target.value
-        })
+class Form extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            origin: '',
+            destiny: '',
+            latorigin: '',
+            longorigin: '',
+            latdestiny: '',
+            longdestiny: ''
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
-    
-    const {origin, destiny} = coordenada
-    
-        // Cuando el usuario presiona agregar cita
-        const submitCoordenada = e => {
-            e.preventDefault();
-    
-            // Validar
-            if(origin.trim() === '' || destiny.trim() === ''){
-                actualizarError(true);
-                return;
-            }
-            // Eliminar el mensaje previo 
-            actualizarError(false);        
-    
-    
-            // Crear la cita
-           crearCoordenada(coordenada.lat, coordenada.lng)
-    
-            // Reiniciar el form
-            actualizarCoordenada({
-                origin: '',
-                destiny: ''
-            })
 
-            /*await axios.get(`.../{origin}/{destiny}`)
-            .then(res => {
-                if(res.data.data.length === 0){
-                    response.val = false
-                } else {
-                    response.ida = res.data.data[0].iata_code
-                }
-            })
-            await axios.get(`.../{origin}/{destiny}`)
-            .then(res => {
-                if(res.data.data.length === 0){
-                    response.val = false
-                } else {
-                    response.ida = res.data.data[0].iata_code
-                }
-            })*/
+    handleInputChange(event) {
+        
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
         }
-
     
+    handleSubmit(e) {
+        e.preventDefault();
+        let origin = this.state.origin;
+        let destiny = this.state.destiny;
+        console.log(origin)
+        console.log(destiny)
+        
+
+        fetch(`http://prubeapi.herokuapp.com/${origin}/${destiny}`, {
+            method: "GET"
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+                this.setState({ ...this.state, latorigin: data.bbox[0], longorigin: data.bbox[1], latdestiny: data.bbox[2], longdestiny: data.bbox[3] });
+        }).catch((error) => {
+            console.error('Error:', error);
+        });
+
+        let latorigin = this.state.latorigin
+        let longorigin = this.state.longorigin
+        let latdestiny = this.state.latdestiny
+        let longdestiny = this.state.longdestiny
+
+        fetch(`http://prubeapi.herokuapp.com/${latorigin}/${longorigin}/${latdestiny}/${longdestiny}`, {
+            method: "GET"
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+        }).catch((error) => {
+            console.error('Error:', error);
+        });
+
+    }
+
+
+render() {
     return (
         <form className="form"
-        onSubmit={submitCoordenada}>
+        onSubmit={this.handleSubmit.bind(this)}>
             <div className="finder">
-                    <label htmlFor="origin"></label>
-                    <input type="text" id="origin" name="origin" onChange={actualizarState} />
-                    <label htmlFor="destiny"></label>
-                    <input type="text" id="destiny" name="destiny" onChange={actualizarState} />
-                    <button type="submit">Ir</button>
+                <label htmlFor="origin"></label>
+                <input type="text" id="origin" name="origin" value={this.state.origin} onChange={this.handleInputChange} />
+                <label htmlFor="destiny"></label>
+                <input type="text" id="destiny" name="destiny" value={this.state.destiny} onChange={this.handleInputChange} />
+                <button type="submit">Ir</button>
             </div>
         </form>
-    )
+    );
+}
 }
 
-export default Form;
+
+export default Form
--- src/components/structure/thirdWindow/ThirdWindow.js
+++ src/components/structure/thirdWindow/ThirdWindow.js
@@ -12,61 +12,58 @@ class ThirdWindow extends React.Component {
       ubicacion: null,
       lat: "",
       lng: "",
-      zoom: 13,
-      isMapInit: false
+      zoom: 15,
+      isMapInit: false,
     };
     this.handleClickAlert = this.handleClickAlert.bind(this);
     this.handleClickHelp = this.handleClickHelp.bind(this);
   }
 
-
   componentDidMount() {
-
-    navigator.geolocation.getCurrentPosition(function location(position) {
-      this.setState({ ...this.state, lat: position.coords.latitude, lng: position.coords.longitude });
-      console.log(position)
-    }.bind(this));
+    navigator.geolocation.getCurrentPosition(
+      function location(position) {
+        this.setState({
+          ...this.state,
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+        console.log(position);
+      }.bind(this)
+    );
   }
 
-
-
-
-  saveMap = map => {
+  saveMap = (map) => {
     this.map = map;
     this.setState({
-      isMapInit: true
+      isMapInit: true,
     });
-    {/*let coordenadasIniciales = JSON.parse(localStorage.getItem("coordenadas"));
-  if (!coordenadasIniciales) {
-    coordenadasIniciales = [];
-  }
-  //Array de coordenadas
-  const [coordenadas, guardarCoordenadas] = useState(coordenadasIniciales);
-
-
-  //Función que toma las cordenadas actuales
-  const crearCoordenada = (coordenada) => {
-    guardarCoordenadas([...coordenadas, coordenada]);
-  };*/}
   };
 
   handleClickHelp = (e) => {
     e.preventDefault();
     console.log("pulse");
     this.props.history.push("/ayuda");
-
   };
 
   handleClickAlert = (e) => {
     e.preventDefault();
     console.log("pulse");
     this.props.history.push("/alerta");
-
   };
   render() {
     const position = [this.state.lat, this.state.lng];
     return (
       <div>
+        <Form/>
+        <button onClick={this.handleClickHelp} className="help">
+        <img
+            src="img/Help/ButtonHelp/ButtonHelp.svg"
+            src="img/ThirdWindow/PlusCircleGrey/PlusCircleGrey.png"
+            alt="logo"
+            srcSet="img/ThirdWindow/PlusCircleGrey/PlusCircleGrey@2x.png 2x, img/ThirdWindow/PlusCircleGrey/PlusCircleGrey@3x.png 3x"
+            className="add-alert-img"
+          ></img>
+        </button>
         <Map center={position} zoom={this.state.zoom} ref={this.saveMap}>
           <TileLayer
             attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
@@ -74,20 +71,19 @@ class ThirdWindow extends React.Component {
           />
           {this.state.isMapInit && <Routing map={this.map} />}
         </Map>
-        <Form //crearCoordenada={crearCoordenada} 
-        />
-        <button onClick={this.handleClickHelp} className="help">Hola</button>
-        <button onClick={this.handleClickAlert} className="add-alert"><p className="add-alert-p">Añadir alerta</p>
+        <button onClick={this.handleClickAlert} className="add-alert">
+          <p className="add-alert-p">Añadir alerta</p>
           <img
             src="img/ThirdWindow/PlusCircleGrey/PlusCircleGrey.svg"
             src="img/ThirdWindow/PlusCircleGrey/PlusCircleGrey.png"
             alt="logo"
             srcSet="img/ThirdWindow/PlusCircleGrey/PlusCircleGrey@2x.png 2x, img/ThirdWindow/PlusCircleGrey/PlusCircleGrey@3x.png 3x"
             className="add-alert-img"
-          ></img></button>
+          ></img>
+        </button>
       </div>
     );
   }
 }
 
-export default ThirdWindow
+export default ThirdWindow;
