823f5551c86ff04d79cf6f92328830382b87b3a5
Silvia <silope0302@gmail.com>
2020-07-21 11:22:31 +0200

    pinta camino

--- src/components/routing/Routing.js
+++ src/components/routing/Routing.js
@@ -7,14 +7,32 @@ import './Routing.css'
 
 class Routing extends MapLayer {
   createLeafletElement() {
-    const { map, resultapi } = this.props;
+    const map = this.props.map;
+    const resultapi = this.props.resultapi;
     console.log(map)
+    console.log(this.props);
     console.log(resultapi)
+    let result;
+    
+    if (this.props.resultapi !== '') {
+      result = resultapi.map(item => L.latLng(item[1], item[0]))
+    }
+    else {
+      result = [];
+    }
+
+    //console.log(resultapi)
     let leafletElement = L.Routing.control({
-      waypoints: [ 
-]
+      //waypoints: [L.latLng(40.4233784, -3.692763),L.latLng(40.44, -3.70), L.latLng(40.43, -3.70)]
+      waypoints: result
     }).addTo(map.leafletElement);
+
     return leafletElement.getPlan();
   }
+
+  componentDidUpdate() {
+    this.createLeafletElement()
+  }
+
 }
 export default withLeaflet(Routing);
